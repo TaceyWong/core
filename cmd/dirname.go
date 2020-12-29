8a92a2735cde0290833a3ea72618ea0fb971be0b
Tacey Wong <xinyong.wang@qq.com>
2020-12-30 00:34:40 +0800

    dirname command completed

--- cmd/dirname.go
+++ cmd/dirname.go
@@ -1,7 +1,9 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
+	"path/filepath"
 
 	"github.com/urfave/cli/v2"
 )
@@ -11,9 +13,10 @@ const DirNameCMDVersion = "0.0.1"
 
 // DirNameCMD define dirname cmd
 var DirNameCMD = cli.Command{
-	Name:    "dirname",
-	Aliases: []string{"DIRNAME"},
-	Usage:   "Output each NAME with its last non-slash component and trailing slashes removed",
+	Name:      "dirname",
+	Aliases:   []string{"DIRNAME"},
+	UsageText: "core dirname [选项] 名称...",
+	Usage:     "Output each NAME with its last non-slash component and trailing slashes removed",
 	Description: `Output each NAME with its last non-slash component and trailing slashes
 	removed; if NAME contains no /'s, output '.' (meaning the current directory).
 	
@@ -26,12 +29,26 @@ var DirNameCMD = cli.Command{
 			Name:    "version",
 			Aliases: []string{"v"},
 			Usage:   "输出版本信息并推出",
+		}, &cli.BoolFlag{
+			Name:    "zero",
+			Aliases: []string{"z"},
+			Usage:   "end each output line with NUL, not newline",
 		},
 	},
 	Action: func(c *cli.Context) error {
 		if c.Bool("version") {
 			fmt.Println(DirNameCMDVersion)
 			return nil
+		}
+		if c.Args().Len() == 0 {
+			return errors.New("缺少操作对象")
+		}
+		end := "\n"
+		if c.Bool("zero") {
+			end = ""
+		}
+		for _, p := range c.Args().Slice() {
+			fmt.Printf("%s%s", filepath.Dir(p), end)
 		}
 		return nil
 	},
