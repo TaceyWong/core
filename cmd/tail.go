780b6f9e5e5f97d9a4b5f44d9c3c0c811c75a064
TaceyWong <xinyong.wang@xtalpi.com>
2020-12-31 13:34:59 +0800

    add tail flags

--- cmd/tail.go
+++ cmd/tail.go
@@ -29,13 +29,68 @@ tail 定期追踪打开给定名称的文件，以确认它是否被删除或被
 			Name:    "version",
 			Aliases: []string{"v"},
 			Usage:   "输出版本信息并推出",
+		}, &cli.IntFlag{
+			Name:    "bytes",
+			Aliases: []string{"c"},
+			Usage:   "output the last NUM bytes; or use -c +NUM to output starting with byte NUM of each file",
+		}, &cli.BoolFlag{
+			Name:    "follow",
+			Aliases: []string{"f"},
+			Usage:   "output appended data as the file grows'",
+		}, & cli.BoolFlag{
+			Name:    "follow-at",
+			Aliases: []string{"x"},
+			Usage:   "output appended data as the file grows; an absent option argument means '`descriptor`'",
+		}, &cli.BoolFlag{
+			Name:  "F",
+			Usage: "same as --follow-at=name --retry",
+		}, &cli.StringFlag{
+			Name:    "lines",
+			Aliases: []string{"n"},
+			Usage:   "output the last NUM lines, instead of the last 10;or use -n +NUM to output starting with line NUM",
+		}, &cli.IntFlag{
+			Name:  "max-unchanged-stats",
+			Value: 5,
+			Usage: "with --follow=name, reopen a FILE which has not" +
+				"changed size after `N` (default 5) iterations" +
+				"to see if it has been unlinked or renamed" +
+				"(this is the usual case of rotated log files);" +
+				"with inotify, this option is rarely useful",
+		}, &cli.IntFlag{
+			Name:  "pid",
+			Usage: "with -f, terminate after process ID, PID dies",
+		}, &cli.BoolFlag{
+			Name:    "quiet",
+			Aliases: []string{"q"},
+			Usage:   "never output headers giving file names",
+		}, &cli.BoolFlag{
+			Name:  "silent",
+			Usage: "same as --quiet",
+		}, &cli.BoolFlag{
+			Name:  "retry",
+			Usage: "keep trying to open a file if it is inaccessible",
+		}, &cli.Float64Flag{
+			Name:    "sleep-interval",
+			Aliases: []string{"s"},
+			Value:   1.0,
+			Usage: "with -f, sleep for approximately `N` seconds between iterations" +
+				"with inotify and --pid=P, check process P at least once every N seconds",
+		}, &cli.BoolFlag{
+			Name:    "verbose",
+			Aliases: []string{"V"},
+			Usage:   "always output headers giving file names",
+		}, &cli.BoolFlag{
+			Name:    "zero-terminated",
+			Aliases: []string{"z"},
+			Usage:   "line delimiter is NUL, not newline",
 		},
 	},
-	Action: func(c *cli.Context) error {
+	Action: func(c *cli.Context) (err error) {
 		if c.Bool("version") {
 			fmt.Println(TailCMDVersion)
 			return nil
 		}
-		return nil
+		fmt.Println(c.FlagNames())
+		return err
 	},
 }
